17299d36ea0bf6144b5e3056980821384156714f
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2014-04-15 17:42:05 +0200

    Adding branch, package and template class for feature #2 .

---
+++ src/main/java/be/kuleuven/cs/flexsim/domain/finances/FinanceTracker.java
@@ -0,0 +1,10 @@
+package be.kuleuven.cs.flexsim.domain.finances;
+
+/**
+ * Tracks and finalizes the finances of productionlines.
+ * 
+ * @author Kristof Coninx <kristof.coninx AT cs.kuleuven.be>
+ * 
+ */
+public class FinanceTracker {
+}
