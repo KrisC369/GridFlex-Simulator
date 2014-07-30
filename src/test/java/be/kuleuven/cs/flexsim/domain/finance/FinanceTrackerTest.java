4e8dd95848d870cf6b75ee5fa62560b0d4b5813b
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2014-07-30 17:51:52 +0200

    Added Test for aggregate finance tracker.

--- src/test/java/be/kuleuven/cs/flexsim/domain/finance/FinanceTrackerTest.java
+++ src/test/java/be/kuleuven/cs/flexsim/domain/finance/FinanceTrackerTest.java
@@ -14,13 +14,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import be.kuleuven.cs.flexsim.domain.finance.DebtModel;
-import be.kuleuven.cs.flexsim.domain.finance.FinanceTrackerImpl;
-import be.kuleuven.cs.flexsim.domain.finance.RewardModel;
 import be.kuleuven.cs.flexsim.domain.process.ProductionLine;
-import be.kuleuven.cs.flexsim.domain.process.TrackableFlexProcessComponent;
 import be.kuleuven.cs.flexsim.domain.process.ProductionLine.ProductionLineBuilder;
 import be.kuleuven.cs.flexsim.domain.process.ProductionLineTest.ChangeEventComponent;
+import be.kuleuven.cs.flexsim.domain.process.TrackableFlexProcessComponent;
 import be.kuleuven.cs.flexsim.domain.resource.Resource;
 import be.kuleuven.cs.flexsim.domain.resource.ResourceFactory;
 import be.kuleuven.cs.flexsim.simulation.SimulationComponent;
@@ -113,6 +110,35 @@ public class FinanceTrackerTest {
         int reward = t.getTotalReward();
         int cost = t.getTotalCost();
         assertEquals(reward - cost, t.getTotalProfit());
+    }
+
+    @Test
+    public void testAggregateSums() {
+        ProductionLine mockPL = new ProductionLineBuilder().addShifted(1)
+                .build();
+        ProductionLine mockPL2 = new ProductionLineBuilder().addShifted(1)
+                .build();
+        FinanceTrackerImpl t = FinanceTrackerImpl.createDefault(mockPL);
+        FinanceTrackerImpl t2 = FinanceTrackerImpl.createDefault(mockPL2);
+        int n = 3;
+        List<Resource> res = ResourceFactory.createBulkMPResource(n, 3, 1);
+        mockPL.deliverResources(res);
+        res = ResourceFactory.createBulkMPResource(n, 3, 1);
+        mockPL.deliverResources(res);
+        sim.register(t);
+        sim.register(t2);
+        FinanceTracker ta = FinanceTrackerImpl.createAggregate(t, t2);
+        assertEquals(0, t.getTotalReward(), 0);
+        ((Simulator) sim).start();
+        int reward = t.getTotalReward();
+        int cost = t.getTotalCost();
+        int reward2 = t2.getTotalReward();
+        int cost2 = t2.getTotalCost();
+        int profit = t.getTotalProfit();
+        int profit2 = t2.getTotalProfit();
+        assertEquals(cost + cost2, ta.getTotalCost(), 1);
+        assertEquals(reward + reward2, ta.getTotalReward(), 1);
+        assertEquals(profit + profit2, ta.getTotalProfit(), 1);
     }
 
     @Test
