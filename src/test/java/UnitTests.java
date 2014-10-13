25ee0796d3ad99a8ddc956ea532a0f09ef477e23
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2014-10-13 17:13:41 +0200

    added class to testrunner.

--- src/test/java/UnitTests.java
+++ src/test/java/UnitTests.java
@@ -9,6 +9,7 @@ import be.kuleuven.cs.flexsim.domain.energy.tso.RandomTSOTest;
 import be.kuleuven.cs.flexsim.domain.finance.FinanceTrackerTest;
 import be.kuleuven.cs.flexsim.domain.process.ProductionLineTest;
 import be.kuleuven.cs.flexsim.domain.resource.ResourceTest;
+import be.kuleuven.cs.flexsim.domain.site.SiteSimulationTest;
 import be.kuleuven.cs.flexsim.domain.site.SiteTest;
 import be.kuleuven.cs.flexsim.domain.util.BufferTest;
 import be.kuleuven.cs.flexsim.domain.util.NPermuteAndCombinerTest;
@@ -24,6 +25,6 @@ import be.kuleuven.cs.flexsim.time.ClockTest;
         FlexTupleTest.class, SiteTest.class, AggregatorImplTest.class,
         RandomTSOTest.class, NPermuteAndCombinerTest.class,
         CopperPlateTSOTest.class, ListenerTest.class, EventTest.class,
-        OutputGeneratorTest.class })
+        OutputGeneratorTest.class, SiteSimulationTest.class })
 public class UnitTests {
 }
