37449e8535d81eba06f1653e294b27492f84026d
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2015-02-04 15:26:33 +0100

    Removed a backwards compat test.

--- src/test/java/IntegrationTests.java
+++ src/test/java/IntegrationTests.java
@@ -2,10 +2,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import be.kuleuven.cs.flexsim.domain.energy.tso.TSOBackwardsCompatibilityTest;
 import be.kuleuven.cs.gametheory.GameTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TSOBackwardsCompatibilityTest.class, GameTest.class })
+@SuiteClasses({ GameTest.class })
 public class IntegrationTests {
 }
