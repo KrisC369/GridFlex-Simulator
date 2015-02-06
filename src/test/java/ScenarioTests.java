287d5d3cf5f67e57e21429fead17176e0b95c3a6
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2015-02-06 12:23:32 +0100

    Bringing back post staging changes.

--- src/test/java/ScenarioTests.java
+++ src/test/java/ScenarioTests.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import be.kuleuven.cs.flexsim.domain.aggregation.AggregationStrategyImpl;
+import be.kuleuven.cs.flexsim.domain.aggregation.Aggregator;
 import be.kuleuven.cs.flexsim.domain.aggregation.IndependentAggregator;
 import be.kuleuven.cs.flexsim.domain.aggregation.ReactiveMechanismAggregator;
 import be.kuleuven.cs.flexsim.domain.energy.generation.ConstantOutputGenerator;
@@ -31,6 +32,9 @@ import be.kuleuven.cs.flexsim.domain.site.Site;
 import be.kuleuven.cs.flexsim.domain.site.SiteImpl;
 import be.kuleuven.cs.flexsim.simulation.SimulationComponent;
 import be.kuleuven.cs.flexsim.simulation.Simulator;
+import be.kuleuven.cs.gametheory.Game;
+import be.kuleuven.cs.gametheory.experimentation.GameConfiguratorEx;
+import be.kuleuven.cs.gametheory.experimentation.io.GameResultWriter;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
@@ -904,4 +908,17 @@ public class ScenarioTests {
         assertEquals(profitBefore, profitAfter, 0);
         return profitAfter - profitBefore;
     }
+
+    @Test
+    public void testGameScenarioExample1() {
+        GameConfiguratorEx ex = new GameConfiguratorEx(0);
+        Game<Site, Aggregator> g = new Game<>(3, ex, 20);
+        g.runExperiment();
+        new GameResultWriter<>(g, "CONSOLE").write();
+        assertTrue(g.getFormattedResultString().contains(
+                "C:PayoffEntry [0, 3]->20"));
+        assertTrue(g.getFormattedResultString().contains(
+                "V:PayoffEntry [0, 3]->[729632.5, 763297.5, 678570.0]"));
+    }
+
 }
