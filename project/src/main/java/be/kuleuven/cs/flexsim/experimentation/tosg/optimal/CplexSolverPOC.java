faad108feb1806b0566b8d22b08852162f1fdc34
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-08-08 21:50:30 +0200

    Added Gurobi solver proof of concept code.

--- project/src/main/java/be/kuleuven/cs/flexsim/experimentation/tosg/optimal/CplexSolverPOC.java
+++ project/src/main/java/be/kuleuven/cs/flexsim/experimentation/tosg/optimal/CplexSolverPOC.java
@@ -6,6 +6,7 @@ import net.sf.jmpi.main.MpResult;
 import net.sf.jmpi.main.MpSolver;
 import net.sf.jmpi.main.expression.MpExpr;
 import net.sf.jmpi.solver.cplex.SolverCPLEX;
+import net.sf.jmpi.solver.gurobi.SolverGurobi;
 
 import static net.sf.jmpi.main.expression.MpExpr.prod;
 import static net.sf.jmpi.main.expression.MpExpr.sum;
@@ -23,13 +24,29 @@ public class CplexSolverPOC {
         MpExpr obj = sum(prod(143, "x"), prod(60, "y"));
         prob.setObjective(obj, MpDirection.MAX);
 
-        prob.add(sum(prod(100, "x"), prod(20, "x"), prod(210, "y")), "<=", 15000);
-        prob.add(sum(prod(110, "x"), prod(30, "y")), "<=", 4000);
+        prob.add(sum(prod(100, "x"), prod(20, "x"), prod(210, "y")), "<=", 1500000);
+        prob.add(sum(prod(110, "x"), prod(30, "y")), "<=", 400000);
         prob.add(sum("x"), "<=", sum(75, prod(-1, "y")));
 
         solver.add(prob);
 
         MpResult result = solver.solve();
         System.out.println(result);
+
+        solver = new SolverGurobi();
+        prob = new MpProblem();
+        prob.addVar("x", Integer.class);
+        prob.addVar("y", Integer.class);
+        obj = sum(prod(143, "x"), prod(60, "y"));
+        prob.setObjective(obj, MpDirection.MAX);
+
+        prob.add(sum(prod(100, "x"), prod(20, "x"), prod(210, "y")), "<=", 1500000);
+        prob.add(sum(prod(110, "x"), prod(30, "y")), "<=", 400000);
+        prob.add(sum("x"), "<=", sum(75, prod(-1, "y")));
+
+        solver.add(prob);
+        result = solver.solve();
+        System.out.println(result);
+        System.out.println(prob);
     }
 }
