b1afae636eee4c2728dd18359c16b432bb818f98
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-09-21 18:24:09 +0200

    Fixed runner

--- project/runSimTosg.sh
+++ project/runSimTosg.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-TARGET=tosg.poc.PoCRunner
+TARGET=tosg.WgmfGameRunner
 REPS=$1
 AGENTS=$2
+SOLVER=GUROBI
 EXTRAPATH=/opt/ibm/ILOG/CPLEX_Studio1262/cplex/bin/x86-64_linux/:/opt/gurobi652/linux64/lib/
 MAVEN_OPTS=-Djava.library.path=$EXTRAPATH
 export MAVEN_OPTS
-mvn exec:java  -DargLine="-Djava.library.path=$EXTRAPATH" -Dexec.mainClass=be.kuleuven.cs.flexsim.experimentation.$TARGET -Dexec.classpathScope=runtime -Dexec.args="$REPS $AGENTS"
+mvn -q clean
+mvn -q compile
+mvn exec:java -DargLine="-Djava.library.path=$EXTRAPATH" -Dexec.mainClass=be.kuleuven.cs.flexsim.experimentation.$TARGET -Dexec.classpathScope=runtime -Dexec.args="-r $REPS -n $AGENTS -s $SOLVER"
