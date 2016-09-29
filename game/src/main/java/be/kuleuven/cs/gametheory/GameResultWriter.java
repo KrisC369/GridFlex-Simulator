dab386c835d8189b7fc0acb285a072fbf4f68573
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-09-29 21:35:22 +0200

    Fixed package cycle.

--- game/src/main/java/be/kuleuven/cs/gametheory/GameResultWriter.java
+++ game/src/main/java/be/kuleuven/cs/gametheory/GameResultWriter.java
@@ -9,15 +9,14 @@ import be.kuleuven.cs.gametheory.standalone.GameDirector;
  * @author Kristof Coninx (kristof.coninx AT cs.kuleuven.be)
  */
 public class GameResultWriter extends ResultWriter {
-    private final GameDirector g;
+    private final AbstractGameDirector g;
 
     /**
      * Default constructor.
      *
-     * @param target
-     *            the game target.
+     * @param target the game target.
      */
-    public GameResultWriter(final GameDirector target) {
+    public GameResultWriter(final AbstractGameDirector target) {
         super(target.getFormattedResults());
         this.g = target;
     }
@@ -25,10 +24,8 @@ public class GameResultWriter extends ResultWriter {
     /**
      * Constructor for filenames.
      *
-     * @param target
-     *            the game target.
-     * @param loggerTag
-     *            The result writer tag.
+     * @param target    the game target.
+     * @param loggerTag The result writer tag.
      */
     public GameResultWriter(final GameDirector target, final String loggerTag) {
         super(target.getFormattedResults(), loggerTag);
