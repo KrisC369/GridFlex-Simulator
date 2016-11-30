6ba896eff02c6899252e6c8206ba4f293ed65cb2
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-11-30 18:50:30 +0100

    Reverted test setup.

--- project/src/test/java/be/kuleuven/cs/flexsim/experimentation/tosg/jppf/WgmfGameRunnerVariableDistributionCostsTest.java
+++ project/src/test/java/be/kuleuven/cs/flexsim/experimentation/tosg/jppf/WgmfGameRunnerVariableDistributionCostsTest.java
@@ -39,7 +39,7 @@ public class WgmfGameRunnerVariableDistributionCostsTest {
 
     @Before
     public void setUp() throws Exception {
-        experimentParams = getParams("OPTA_BEST_EFFORT");
+        experimentParams = getParams("DUMMY");
         runner = new WgmfGameRunnerVariableDistributionCosts(experimentParams);
     }
 
