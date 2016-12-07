0a51ca9949b37f4d5cc8dc97527ce898fb817c9f
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-12-07 17:58:57 +0100

    Added tests.

---
+++ project/src/test/java/be/kuleuven/cs/flexsim/experimentation/tosg/data/CsvResultWriterTest.java
@@ -0,0 +1,34 @@
+package be.kuleuven.cs.flexsim.experimentation.tosg.data;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Kristof Coninx <kristof.coninx AT cs.kuleuven.be>
+ */
+public class CsvResultWriterTest {
+
+    @Test
+    public void writeCsvFile() throws Exception {
+        double[] fixedPoints = new double[] { 0.2, 0.3 };
+        double pp = 43.2;
+        String file = "testOut.csv";
+        CsvResultWriter.WgmfDynamicsResults wgmfDynamicsResults = CsvResultWriter
+                .WgmfDynamicsResults
+                .create(2, pp, fixedPoints, fixedPoints, fixedPoints);
+        List<CsvResultWriter.WgmfDynamicsResults> results = Lists
+                .newArrayList(wgmfDynamicsResults, wgmfDynamicsResults);
+        CsvResultWriter.writeCsvFile(file, results);
+
+        File varTmpDir = new File(file);
+        boolean exists = varTmpDir.exists();
+        assertTrue(exists);
+        varTmpDir.delete();
+    }
+
+}
