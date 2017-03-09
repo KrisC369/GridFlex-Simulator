464ea0ed6e0650ab739622d1bade0f5337571a2b
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2017-03-09 12:47:53 +0100

    Added file initializer.

---
+++ project/src/main/java/be/kuleuven/cs/flexsim/experimentation/tosg/utils/WriteFileCreator.java
@@ -0,0 +1,30 @@
+package be.kuleuven.cs.flexsim.experimentation.tosg.utils;
+
+import be.kuleuven.cs.flexsim.persistence.MapDBMemoizationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Kristof Coninx <kristof.coninx AT cs.kuleuven.be>
+ */
+public class WriteFileCreator {
+
+    private static final int SLEEP_TIME = 200;
+    private static final Logger logger = LoggerFactory.getLogger(WriteFileCreator.class);
+
+    public static void main(String[] args) {
+        for (String file : args) {
+            logger.info("Writing ouput caching files for {}", file);
+            MapDBMemoizationContext.createDefaultEnsureFileExists(file);
+            sleep(SLEEP_TIME);
+        }
+    }
+
+    private static void sleep(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
