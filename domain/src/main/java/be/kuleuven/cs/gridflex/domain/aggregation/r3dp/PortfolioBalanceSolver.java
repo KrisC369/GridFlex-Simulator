17ce4567d9954f5e09064904d9472edf0c6ebe73
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2017-05-11 11:14:51 +0200

    Implemented strategy for converting input data to wanted profile format.

--- domain/src/main/java/be/kuleuven/cs/gridflex/domain/aggregation/r3dp/PortfolioBalanceSolver.java
+++ domain/src/main/java/be/kuleuven/cs/gridflex/domain/aggregation/r3dp/PortfolioBalanceSolver.java
@@ -8,6 +8,8 @@ import be.kuleuven.cs.gridflex.domain.util.data.profiles.CongestionProfile;
 
 import java.util.List;
 
+import static be.kuleuven.cs.gridflex.domain.aggregation.r3dp.PortfolioBalanceSolver
+        .ProfileConversionStrategy.WINDSPEED_ERROR_BASED;
 import static org.apache.commons.math3.util.FastMath.min;
 
 /**
@@ -29,24 +31,33 @@ public class PortfolioBalanceSolver extends AbstractFlexAllocationSolver {
      */
     public PortfolioBalanceSolver(AbstractSolverFactory<SolutionResults> fac,
             SolverInputData inputData) {
+        this(fac, inputData, WINDSPEED_ERROR_BASED);
+    }
+
+    /**
+     * Default constructor
+     *
+     * @param fac       The solvers factory to draw solvers from.
+     * @param inputData the input data.
+     * @param strategy  the strategy for converting the input data to congestion profiles.
+     */
+    public PortfolioBalanceSolver(AbstractSolverFactory<SolutionResults> fac,
+            SolverInputData inputData, ProfileConversionStrategy strategy) {
         super(fac);
         this.budget = BudgetTracker
                 .createDayAheadSellingPrice(inputData.getPositiveImbalancePriceProfile(),
                         inputData.getDayAheadPriceProfile());
-        this.congestion = applyWindForecastErrorAndBudgetConstraints(inputData);
+        //        this.congestion = applyWindForecastErrorAndBudgetConstraints(inputData);
+        this.congestion = applyBudgetConstraintFilters(strategy.applyConversion(inputData),
+                inputData);
     }
 
-    private CongestionProfile applyWindForecastErrorAndBudgetConstraints(SolverInputData input) {
-        MultiHorizonErrorGenerator gen = new MultiHorizonErrorGenerator(input.getSeed(),
-                input.getForecastHorizonErrorDistribution());
-        CongestionProfile profile = new TurbineProfileConvertor(input.getCableCurrentProfile(),
-                input.getTurbineSpecifications(), gen)
-                .convertProfileTPositiveOnlyoImbalanceVolumes();
+    private CongestionProfile applyBudgetConstraintFilters(CongestionProfile profile,
+            SolverInputData input) {
         //Only neg NRV should be corrected.
         CongestionProfile negOnly = profile
                 .transformFromIndex(i -> input.getNetRegulatedVolumeProfile().value(i) < 0 ?
-                        profile.value(i) :
-                        0);
+                        profile.value(i) : 0);
         //Only positive budgets are useful.
         return negOnly
                 .transformFromIndex(i -> budget.getBudgetForPeriod(i) < 0 ? 0 : profile.value(i));
@@ -74,5 +85,37 @@ public class PortfolioBalanceSolver extends AbstractFlexAllocationSolver {
     @Override
     public TimeSeries getCongestionVolumeToResolve() {
         return this.congestion;
+    }
+
+    public enum ProfileConversionStrategy {
+        /**
+         * Windspeed forecast based error conversion makes use of the turbine convertor to
+         * deaggregate wind production data to single turbine level, inferring the corresponding
+         * wind speeds and then applying the wind forecast error data on the wind speeds before
+         * converting the data back to aggregated energy volumes.
+         */
+        WINDSPEED_ERROR_BASED {
+            @Override
+            public CongestionProfile applyConversion(SolverInputData input) {
+                MultiHorizonErrorGenerator gen = new MultiHorizonErrorGenerator(input.getSeed(),
+                        input.getForecastHorizonErrorDistribution());
+                return new TurbineProfileConvertor(input.getCableCurrentProfile(),
+                        input.getTurbineSpecifications(), gen)
+                        .convertProfileTPositiveOnlyoImbalanceVolumes();
+            }
+        },
+        /**
+         * Power forecast error based conversion applies error data directly on the nominal power
+         * production rates.
+         */
+        POWER_ERROR_BASED {
+            @Override
+            public CongestionProfile applyConversion(SolverInputData input) {
+                //// TODO: 11/05/17 complete conversion.
+                return null;
+            }
+        };
+
+        abstract public CongestionProfile applyConversion(SolverInputData input);
     }
 }
