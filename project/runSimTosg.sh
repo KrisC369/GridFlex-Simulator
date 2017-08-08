1dfa7e57d1d0541fa4cdb1a5b27366054d8c88b6
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2017-08-08 16:31:50 +0200

    Fixed broken run script.

--- project/runSimTosg.sh
+++ project/runSimTosg.sh
@@ -12,10 +12,10 @@ DATAPROF=$7
 DIST=$8
 EXTRAOPTS="-c u -flexIA 12 -flexDUR 2 -flexCOUNT 40"
 EXTRAPATH=/localhost/packages/gurobi/gurobi701/linux64/lib/:/home/u0091633/local/ibm/ILOG/CPLEX_Studio1263/cplex/bin/x86-64_linux/
-JPPF_CONFIG_PATH=target/classes/be/kuleuven/cs/flexsim/experimentation/configs/jppf-client.properties
-LOG4J_CONFIG_PATH=target/classes/be/kuleuven/cs/flexsim/experimentation/configs/log4j.properties
+JPPF_CONFIG_PATH=target/classes/be/kuleuven/cs/gridflex/experimentation/configs/jppf-client.properties
+LOG4J_CONFIG_PATH=target/classes/be/kuleuven/cs/gridflex/experimentation/configs/log4j.properties
 MAVEN_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=1098 -Djava.library.path=$EXTRAPATH -Xmx8g -Djppf.config=$JPPF_CONFIG_PATH -Dlog4j.configuration=file:$LOG4J_CONFIG_PATH"
 export MAVEN_OPTS
 mvn -q clean
 mvn -q compile
-nice -n 19 mvn exec:java -Dexec.mainClass=be.kuleuven.cs.flexsim.experimentation.$TARGET -Dexec.classpathScope=runtime -Dexec.cleanupDaemonThreads=false -Dexec.args="-r $REPS -n $AGENTS -s $SOLVER -m $MODE -p1start $P1START -p1step $P1STEP -p1end $P1STOP -dIdx $ERRORPROF -pIdx $DATAPROF -distribution $DIST $EXTRAOPTS"
+nice -n 19 mvn exec:java -Dexec.mainClass=be.kuleuven.cs.gridflex.experimentation.$TARGET -Dexec.classpathScope=runtime -Dexec.cleanupDaemonThreads=false -Dexec.args="-r $REPS -n $AGENTS -s $SOLVER -m $MODE -p1start $P1START -p1step $P1STEP -p1end $P1STOP -dIdx $ERRORPROF -pIdx $DATAPROF -distribution $DIST $EXTRAOPTS"
