1df1a9bdde300f8cf36b1c20227c12f9b07adda9
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2017-08-28 11:04:27 +0200

    added serialization marker.

--- project/src/main/java/be/kuleuven/cs/gridflex/experimentation/tosg/wgmf/OptaExperimentResults.java
+++ project/src/main/java/be/kuleuven/cs/gridflex/experimentation/tosg/wgmf/OptaExperimentResults.java
@@ -3,13 +3,14 @@ package be.kuleuven.cs.gridflex.experimentation.tosg.wgmf;
 import be.kuleuven.cs.gridflex.domain.energy.dso.r3dp.HourlyFlexConstraints;
 import com.google.auto.value.AutoValue;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 /**
  * @author Kristof Coninx <kristof.coninx AT cs.kuleuven.be>
  */
 @AutoValue
-public abstract class OptaExperimentResults {
+public abstract class OptaExperimentResults implements Serializable {
     public abstract BigDecimal getResultValue();
 
     public abstract HourlyFlexConstraints getFlexConstraints();
